5a263ee401f094e55bd448d70c6e9ac6f575e505
BarisPY <celikbaris263@gmail.com>
2024-09-05 14:49:52 +0300

    Update facebaris.py

--- facebaris.py
+++ facebaris.py
@@ -1,60 +1,60 @@
 import cv2
 import os
 
-# Kamera numarası (0, 1, 2, ...) - Birden fazla kamera varsa değiştirilebilir
+# Camera number (0, 1, 2, ...) - can be changed if there is more than one camera
 camera_number = 0
 
-# Haar Cascade yüz sınıflandırıcısı
+# Haar Cascade face classifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Kamera açma
+# Camera opening
 cap = cv2.VideoCapture(camera_number)
 
-# Klasörü oluştur
+# Create folder
 output_folder = 'faces'
 os.makedirs(output_folder, exist_ok=True)
 
-# Tanınan yüz sayısı
+# Number of recognised faces
 recognized_faces = set()
 
 while True:
-    # Kameradan bir kare al
+    # Take a frame from the camera
     ret, frame = cap.read()
 
-    # Gri tona dönüştür
+    # Convert to grey tone
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Yüzleri tespit et
+    # Detect faces
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-    # Her yüzü işle
+    # Process each face
     for (x, y, w, h) in faces:
-        # Yüzü kırp
+        # Crop the face
         face_img = frame[y:y+h, x:x+w]
 
-        # Yüzü tanıma kontrolü
+        # Face recognition check
         face_hash = hash(face_img.tobytes())
         if face_hash not in recognized_faces:
-            # Tanınan yüzü kaydet
+            # Save the recognisable face
             img_name = os.path.join(output_folder, f'{len(recognized_faces) + 1}.jpg')
 
-            # Yüzü işaretle
+            # Mark the face
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(frame, 'Face', (x, y-10), font, 0.9, (0, 0, 255), 2)
 
-            # Görüntüyü tam ekran kaydet
+            # Save image full screen
             cv2.imwrite(img_name, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
             recognized_faces.add(face_hash)
 
-    # Kameradan alınan görüntüyü tam ekran göster
+    # Show the image from the camera full screen
     cv2.imshow('Camera', frame)
 
-    # Çıkış için 'q' tuşuna basın
+    # Press ‘q’ to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Kamera ve pencereyi kapat
+# Close camera and window
 cap.release()
 cv2.destroyAllWindows()
